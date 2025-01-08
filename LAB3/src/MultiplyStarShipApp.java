9143385e75b25171bfab9aed88c47628034a01bb
Mishutkablack <mykhailo.bletsko11@gmail.com>
2025-01-08 22:57:49 +0200

    add resulted codes MultiplyStarShip.java and MultiplyStarShipApp.java

---
+++ LAB3/src/MultiplyStarShipApp.java
@@ -0,0 +1,42 @@
+package Ki32VolokhovLab4;
+
+import Ki32VolokhovLab4.MultiplyStarShip;
+import Ki32VolokhovLab4.MultiplyStarShip.StarShipColor;
+
+public class MultiplyStarShipApp {
+    public static void main(String[] args) {
+        
+        MultiplyStarShip multiplyStarShip=new MultiplyStarShip();
+        System.out.println(multiplyStarShip.getStarShipName());
+        System.out.println(multiplyStarShip.getStarShipColor());
+
+        System.out.println(multiplyStarShip.checkEngineWear());
+        System.out.println(multiplyStarShip.checkFuel());
+
+        multiplyStarShip.refuel();
+        multiplyStarShip.repair();
+
+        System.out.println(multiplyStarShip.checkEngineWear());
+        System.out.println(multiplyStarShip.checkFuel());
+        
+        MultiplyStarShip multiplyStarShip1 = new MultiplyStarShip(
+                "MotherShip",
+                MultiplyStarShip.StarShipColor.RED,  
+                -1,
+                -1
+            );
+        System.out.println(multiplyStarShip1.getStarShipName());
+        System.out.println(multiplyStarShip1.getStarShipColor());
+
+        System.out.println(multiplyStarShip1.checkEngineWear());
+        System.out.println(multiplyStarShip1.checkFuel());
+
+        multiplyStarShip1.refuel();
+        multiplyStarShip1.repair();
+        
+        System.out.println(multiplyStarShip1.checkEngineWear());
+        System.out.println(multiplyStarShip1.checkFuel());
+    
+    }
+}
+
